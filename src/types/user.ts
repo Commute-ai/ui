c697a416d3913233d8d0102ffed41dd7031bafc2
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-13 15:12:05 +0300

    chore: improve user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-    id: z.string(),
+    id: z.number(),
     username: z.string().min(3).max(30),
+    created_at: z.string().refine((date) => !isNaN(Date.parse(date))),
 });
 
 export const NewUserSchema = UserSchema.extend({
     password: z.string().min(6).max(100),
-}).omit({ id: true });
+}).omit({ id: true, created_at: true });
 
 export type User = z.infer<typeof UserSchema>;
 export type NewUser = z.infer<typeof NewUserSchema>;
