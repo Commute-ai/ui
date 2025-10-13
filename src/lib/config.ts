6c9410c49083d2f0c5b3a59236197aaea1e43fe1
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-13 14:42:04 +0300

    chore: improve config.ts and move the envs to eas

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,43 +1,22 @@
 import { Platform } from "react-native";
+import z from "zod";
 
-const IS_DEVELOPMENT = process.env.APP_VARIANT === "development";
-const IS_STAGING = process.env.APP_VARIANT === "staging";
-const IS_PRODUCTION = process.env.APP_VARIANT === "production";
+const ConfigSchema = z.object({
+    appVariant: z.enum(["development", "staging", "production"]),
+    apiUrl: z.string().url(),
+});
 
-const ENV = {
-    development: {
-        web: {
-            apiUrl: "http://localhost:8000/api/v1",
-        },
-        android: {
-            apiUrl: "http://10.0.2.2:8000/api/v1",
-        },
-    },
-    staging: {
-        apiUrl: "https://backend.staging.commute.ai.ender.fi/api/v1",
-    },
-    production: {
-        apiUrl: "https://backend.commute.ai.ender.fi/api/v1",
-    },
-};
+export type Config = z.infer<typeof ConfigSchema>;
 
-const getEnvVars = (): { apiUrl: string } => {
-    if (IS_STAGING) {
-        return ENV.staging;
-    }
-
-    if (IS_PRODUCTION) {
-        return ENV.production;
-    }
-
-    if (IS_DEVELOPMENT) {
-        if (Platform.OS === "android") {
-            return ENV.development.android;
-        }
-        return ENV.development.web;
-    }
-
-    throw new Error("No APP_VARIANT set in environment variables");
+const getEnvVars = (): Config => {
+    return ConfigSchema.parse({
+        appVariant: process.env.APP_VARIANT || "development",
+        apiUrl:
+            process.env.API_URL ||
+            (Platform.OS === "web"
+                ? "http://localhost:8000/api/v1"
+                : "http://10.0.2.2:8000/api/v1"),
+    });
 };
 
 export default getEnvVars();
