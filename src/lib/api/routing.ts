0ebddebbf83e0115c4e66de70a6d50b7b07348df
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-29 16:58:00 +0200

    refacor: remove unecessary ai_description from routesearchresponse

--- src/lib/api/routing.ts
+++ src/lib/api/routing.ts
@@ -43,7 +43,6 @@ const RouteSearchResponseSchema = z.object({
     destination: CoordinatesSchema,
     itineraries: z.array(ItinerarySchema),
     search_time: z.string(),
-    ai_description: z.string().optional().nullable(),
 });
 
 export const routingApi = {
